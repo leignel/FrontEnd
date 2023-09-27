371758c48fd5d1823cff011c3cac52ed637c0de5
leignel <cristian.leignel@gmail.com>
2023-09-27 16:44:47 -0300

    Imagens

---
+++ Aulas/aula-06/js/script.js
@@ -0,0 +1,30 @@
+// alert("Alert no JS Externo");
+
+//console.log("Mensagem de Console");
+
+// 3 formas de declarar variáveis - var, let e const
+// const nome = prompt("Digite seu nome: ");
+// console.log(nome);
+// alert(nome);
+
+// Tipos no JS, Number, String, Boolean, Undefined, Object(null), function
+
+// let idade;
+// console.log(idade);
+
+//Objetos
+
+const pessoa = {
+  nome: "Cristian Leignel",
+  idade: 28,
+  estaVivo: true,
+};
+
+console.log(pessoa);
+
+//ARRAY []
+
+const frutas = ["abacaxo", "maça", "Laranja"];
+console.log(frutas);
+
+//console.log(frutas.length); tamanho do array
